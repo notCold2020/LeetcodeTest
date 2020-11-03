5a453b6c8f75420823e363d329f58d4f1a1baf0b
慈星锐 <252384801@qq.com>
2020-11-03 15:57:37 +0800

    一些写过的demo

--- src/main/java/com/lrw/LeecodetestApplication.java
+++ src/main/java/com/lrw/LeecodetestApplication.java
@@ -3,6 +3,7 @@ package com.lrw;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @MapperScan("com.lrw.ohter.demo.dao")
--- src/main/java/com/lrw/ohter/Solution.java
+++ src/main/java/com/lrw/ohter/Solution.java
@@ -1,16 +1,29 @@
 package com.lrw.ohter;
 
+import com.alibaba.dubbo.config.spring.ReferenceBean;
+import com.alibaba.dubbo.config.spring.ServiceBean;
+import com.alibaba.dubbo.registry.integration.RegistryProtocol;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class Solution {
 
     public static void main(String[] args) {
 
-    }
+//        String format = String.format("%.2f", Double.parseDouble("0.60"));
+//        String format1 = new DecimalFormat("0.00%").format(Double.parseDouble(String.format("%.2f", Double.parseDouble("1.00"))));
+        int i = 0;
+        int a = i++;
+        System.out.println(a);
 
+    }
 
 }
