0a52512ce5ced06710efaa17ecc64aa332c3e80c
慈星锐 <252384801@qq.com>
2020-11-19 19:38:45 +0800

    Revert "revet测试"
    This reverts commit e6868bbf

--- src/main/java/com/cxr/other/Solution.java
+++ src/main/java/com/cxr/other/Solution.java
@@ -1,34 +1,87 @@
 package com.cxr.other;
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 
 import java.io.Serializable;
-import java.util.*;
-
+import java.util.HashMap;
+import java.util.Map;
 
 class Solution {
-    public String longestCommonPrefix(String[] strs) {
-        if (strs == null || strs.length == 0) {
-            return "";
-        }
-        int length = strs[0].length();
-        int count = strs.length;
-        for (int i = 0; i < length; i++) {
-            //得到数组第一个元素的每一个 字母
-            char c = strs[0].charAt(i);
-            for (int j = 1; j < count; j++) {
-                if (i == strs[j].length() || strs[j].charAt(i) != c) {
-                    //公共前缀【0】里面肯定有
-                    return strs[0].substring(0, i);
-                }
-            }
-        }
-        return strs[0];
+    public static void main(String[] args) {
+        SearchCondition searchCondition = JSON.parseObject("{\"key\":\"callId,applyNo\",\"name\":\"user_field\",\"value\":\"1,1\"}\n", SearchCondition.class);
+        System.out.println(searchCondition);
     }
 }
+ class SearchCondition implements Serializable {
 
+
+    private String name;
+
+    private String key;
+
+    private Integer condition;
+
+    private String value;
+
+    // kryo序列化用
+    public SearchCondition() {}
+
+    public SearchCondition(String name, Integer condition, String value) {
+        this.name = name;
+        this.condition = condition;
+        this.value = value;
+    }
+
+    public SearchCondition(String name, String key, String value) {
+        this.name = name;
+        this.key = key;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public Integer getCondition() {
+        return condition;
+    }
+
+    public void setCondition(Integer condition) {
+        this.condition = condition;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("{");
+        sb.append("\"name\":\"")
+                .append(name).append('\"');
+        sb.append(",\"key\":\"")
+                .append(key).append('\"');
+        sb.append(",\"condition\":")
+                .append(condition);
+        sb.append(",\"value\":\"")
+                .append(value).append('\"');
+        sb.append('}');
+        return sb.toString();
+    }
+}
 
 
 
